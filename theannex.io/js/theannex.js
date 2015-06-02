a1b85c7a4cd5da9fa89b072f0a4d27f15a35ccdb
Christopher Kula <cjkula@gmail.com>
2015-06-02 16:37:20 -0700

    Refactor to new file

---
+++ theannex.io/js/theannex.js
@@ -0,0 +1,31 @@
+window.TheAnnex = {
+  
+  IntranetDetector: function(opts) {
+    var $img = jQuery('<img>', {
+          src: "http://ping.johnmcneilstudio.private/pixel.png",
+          style: "position: absolute"
+        });
+
+    $img.one('load', function() {
+      $img.remove();
+      if (opts.connected) {
+        opts.connected.call(this);
+      }
+    });
+
+    $('body').append($img);
+  }
+
+  Carousel: function(opts) {
+    this.init(opts);
+  }
+
+}
+
+TheAnnex.Carousel.prototype = {
+
+  init: function(opts) {
+    alert('hello carousel');
+  }
+
+}
